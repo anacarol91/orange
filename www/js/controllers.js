0c8656fa13c58ac07781ca6f008efbc0a85c81cd
Ana Carolina <anacgp91@gmail.com>
2017-03-13 18:47:37 -0300

    card de posts

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,12 +5,11 @@ angular.module('starter.controllers', [])
 
     // You can change this url to experiment with other endpoints
     var postsApi = 'http://marketingpordados.com/wp-json/wp/v2/posts';
-                    
+
     console.log("Enter PostsCtrl");
 
     $ionicLoading.show();
-    
-   
+
     // This should go in a service so we can reuse it
     $http.get( postsApi ).
       success(function(data, status, headers, config) {
@@ -46,35 +45,88 @@ angular.module('starter.controllers', [])
 
 
 .controller('PostCtrl', function($scope, $stateParams, $sce, $http ) {
- 
+
   // we get the postID from $stateParams.postId, the query the api for that post
   var singlePostApi = 'http://marketingpordados.com/wp-json/wp/v2/posts/' + $stateParams.postId;
- 
+
   $http.get( singlePostApi ).
     success(function(data, status, headers, config) {
       $scope.post = data;
       console.log( data );
- 
+
       // must use trustAsHtml to get raw HTML from WordPress
      // $scope.content = $sce.trustAsHtml(data.content);
- 
+
     }).
     error(function(data, status, headers, config) {
       console.log( 'Single post load error.' );
     });
- 
+
 })
 
 
-.controller('EventosCtrl', function($scope, Eventos) {
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+.controller('EventosCtrl', function($scope, $ionicLoading, $http, Eventos) {
+  $scope.$on('$ionicView.enter', function(e) {
 
-  $scope.eventos = Eventos.all();
+    // You can change this url to experiment with other endpoints
+    var eventosApi = 'http://marketingpordados.com/wp-json/wp/v2/posts?filter[category_name]=eventos';
+
+    console.log("Enter EventosCtrl");
+
+    $ionicLoading.show();
+
+    // This should go in a service so we can reuse it
+    $http.get( eventosApi ).
+      success(function(data, status, headers, config) {
+        $scope.eventos = data;
+        console.log( data );
+
+        $ionicLoading.hide();
+      }).
+      error(function(data, status, headers, config) {
+        console.log( 'Eventos load error.' );
+        $ionicLoading.hide();
+      });
+  });
+
+  $scope.refreshEventos = function () {
+    var eventosApi = 'http://marketingpordados.com/wp-json/wp/v2/posts?filter[category_name]=eventos';
+
+    $ionicLoading.show();
+
+    // This should go in a service so we can reuse it
+    $http.get( eventosApi ).
+      success(function(data, status, headers, config) {
+        $scope.eventos = data;
+        console.log( data );
+
+        $ionicLoading.hide();
+      }).
+      error(function(data, status, headers, config) {
+        console.log( 'Eventos load error.' );
+        $ionicLoading.hide();
+      });
+
+    $scope.$broadcast('scroll.refreshComplete');
+  }
 })
 
-.controller('EventoCtrl', function($scope, $stateParams, Evento) {
-  $scope.evento = Eventos.get($stateParams.eventoId);
+.controller('EventoCtrl', function($scope, $stateParams, $http, Evento) {
+  // we get the postID from $stateParams.postId, the query the api for that post
+  var singleEventoApi = 'http://marketingpordados.com/wp-json/wp/v2/posts?filter[category_name]=eventos/' + $stateParams.eventoId;
+
+  $http.get( singleEventoApi ).
+    success(function(data, status, headers, config) {
+      $scope.evento = data;
+      console.log( data );
+
+      // must use trustAsHtml to get raw HTML from WordPress
+     // $scope.content = $sce.trustAsHtml(data.content);
+
+    }).
+    error(function(data, status, headers, config) {
+      console.log( 'Single event load error.' );
+    });
 })
 
 
